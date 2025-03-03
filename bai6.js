964d33961cc83fbaced0f12e2ba303757e30618d
davidpham26 <phonghaha20065432@gmail.com>
2025-03-03 07:51:01 +0700

    bai6.js

--- bai6.js
+++ bai6.js
@@ -1,6 +1,6 @@
 let year = prompt("Nhập một năm:");
 
-if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+if (year % 4 === 0 && (year % 100 === 0 || year % 400 === 0)) {
     alert(year + " là năm nhuận");
 } else {
     alert(year + " không phải là năm nhuận");
