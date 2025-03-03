932156a07c3777a7c6bd3febf73b893fbefbc6e3
davidpham26 <phonghaha20065432@gmail.com>
2025-03-03 07:54:48 +0700

    bai1.js

--- bai1.js
+++ bai1.js
@@ -1,5 +1,5 @@
 let number = prompt(" nhập một số nguyên:");
-if (number !== null) {
+if (number === null) {
     number = parseInt(number);
     if (is(number)) {
         console.log("Giá trị nhập vào không phải là số nguyên.");
